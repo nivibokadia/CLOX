07aab195c8dca4c9b4d608227ce738f830acafba
nivibokadia <nivibokadia@gmail.com>
2024-06-13 14:51:42 +0530

    Chapter 18: Types of Values

--- value.h
+++ value.h
@@ -2,13 +2,36 @@
 #define clox_value_h 
 #include "common.h" 
 
-typedef double Value;
+typedef enum{          //to differentiate diff types of values
+    VAL_BOOL,
+    VAL_NIL,
+    VAL_NUMBER,
+}ValueType;
+
+typedef struct{
+    ValueType type;
+    union{
+        bool boolean;
+        double number;
+    }as;
+}Value;
+
+#define IS_BOOL(value) ((value).type == VAL_BOOL)
+#define IS_NIL(value) ((value).type == VAL_NIL)
+#define IS_NUMBER(value) ((value).type == VAL_NUMBER)
+#define AS_BOOL(value) ((value).as.boolean)     //to get the value back in C
+#define AS_NUMBER(value) ((value).as.number)
+#define BOOL_VAL(value)     ((Value){VAL_BOOL, {.boolean = value}})     //produces Value from appropiate c value
+#define NIL_VALUE           ((Value){VAL_NIL, {.number = 0}})
+#define NUMBER_VAL(value)   ((Value)){VAL_NUMBER, {.number = value}}
+
 typedef struct{
     int capacity;
     int count;
     Value* values;
 }ValueArray;
 
+bool valuesEqual(Value a, Value b);
 void initValueArray(ValueArray* array);
 void writeValueArray(ValueArray* array, Value values);
 void freeValueArray(ValueArray* array);
--- vm.h
+++ vm.h
@@ -4,14 +4,14 @@
 #include "value.h"
 #define STACK_MAX 256
 
-typedef struct{
-    Chunk* chunk;
-    uint8_t* ip;
-    Value stack[STACK_MAX];
-    Value* stackTop;
-}VM;
+typedef struct {
+    Chunk* chunk;               // The current chunk of bytecode being executed.
+    uint8_t* ip;                // The instruction pointer.
+    Value stack[STACK_MAX];     // The operand stack.
+    Value* stackTop;            // Pointer to the top of the stack.
+} VM;
 
-typedef enum {
+typedef enum {                  //shows the state of VM
  INTERPRET_OK,
  INTERPRET_COMPILE_ERROR,
  INTERPRET_RUNTIME_ERROR
@@ -20,7 +20,7 @@ typedef enum {
 void initVM();
 void freeVM();
 
-InterpretResult interpret(const char* source);
+InterpretResult interpret(const char* source);  //func to set up VM and start executing bytecode
 void push(Value value);
 Value pop();
 
